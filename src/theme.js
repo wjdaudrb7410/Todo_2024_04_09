4793e8c4de67687c498f90aedeb3d9e62b54f698
wjdaudrb7410 <79891522+wjdaudrb7410@users.noreply.github.com>
2024-04-12 08:41:42 +0900

    Init

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/noto-sans-kr";
+const theme = extendTheme({
+  fonts: {
+    heading: '"Noto Sans KR","sans-serif"',
+    body: '"Noto Sans KR","sans-serif"',
+  },
+});
+export default theme;

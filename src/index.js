d6bbeb8df576efef171e99ec82cee32e8cdad28c
wjdaudrb7410 <79891522+wjdaudrb7410@users.noreply.github.com>
2024-04-15 16:11:55 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import { App } from "./App";
-import theme from "./theme";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Me } from "./pages/Home/Home";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";

cb0c6397ee150677ebf3fe38839e98b6a70ec711
wjdaudrb7410 <79891522+wjdaudrb7410@users.noreply.github.com>
2024-04-21 15:14:13 +0900

    다크모드 수정

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 import "@fontsource/noto-sans-kr";
 const theme = extendTheme({
   fonts: {
@@ -8,6 +9,13 @@ const theme = extendTheme({
   config: {
     initialColorMode: "light",
     useSystemColorMode: false,
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: mode("#eee", "#121212")(props),
+      },
+    }),
   },
 });
 export default theme;
